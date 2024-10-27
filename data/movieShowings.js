9ec0e949c68228a4df9c7a143d9ed1c49298db9b
hoken92 <hkenneth92@gmail.com>
2024-10-27 14:06:53 -0700

    added data structure

---
+++ data/movieShowings.js
@@ -0,0 +1,74 @@
+const movieShowings = [
+  {
+    id: 1,
+    movie: "Way, Way Back, The",
+    genre: "Comedy|Drama",
+    showing_date: "10/26/2024",
+    end_date: "11/03/2024",
+  },
+  {
+    id: 2,
+    movie: "Trek Nation",
+    genre: "Documentary",
+    showing_date: "10/26/2024",
+    end_date: "10/31/2024",
+  },
+  {
+    id: 3,
+    movie: "Beats Rhymes & Life: The Travels of a Tribe Called Quest",
+    genre: "Documentary",
+    showing_date: "10/26/2024",
+    end_date: "11/07/2024",
+  },
+  {
+    id: 4,
+    movie: "Frozen",
+    genre: "Adventure|Animation|Comedy|Fantasy|Musical|Romance",
+    showing_date: "10/26/2024",
+    end_date: "10/31/2024",
+  },
+  {
+    id: 5,
+    movie: "Strong Man, The",
+    genre: "Comedy",
+    showing_date: "10/26/2024",
+    end_date: "11/04/2024",
+  },
+  {
+    id: 6,
+    movie: "St. Ives",
+    genre: "Crime|Mystery|Thriller",
+    showing_date: "10/26/2024",
+    end_date: "11/07/2024",
+  },
+  {
+    id: 7,
+    movie: "Vampire Bat, The",
+    genre: "Horror",
+    showing_date: "10/26/2024",
+    end_date: "11/01/2024",
+  },
+  {
+    id: 8,
+    movie: "Triggermen",
+    genre: "Comedy|Crime",
+    showing_date: "10/26/2024",
+    end_date: "11/05/2024",
+  },
+  {
+    id: 9,
+    movie: "Summer House",
+    genre: "Drama",
+    showing_date: "10/26/2024",
+    end_date: "11/03/2024",
+  },
+  {
+    id: 10,
+    movie: "Private Benjamin",
+    genre: "Comedy",
+    showing_date: "10/26/2024",
+    end_date: "11/05/2024",
+  },
+];
+
+module.exports = movieShowings;
